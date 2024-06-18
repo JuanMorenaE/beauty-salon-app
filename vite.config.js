e31558c972344f804e4f37d3cc21b9e48c0f5659
Juan Morena <thewolfmodzyt@gmail.com>
2024-06-17 22:17:52 -0300

    Github base url fixed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwind from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/beauty-salon-app",
+    base: "https://juanmorenae.github.io/beauty-salon-app",
     css: {
         postcss: {
             plugins: [tailwind()],
